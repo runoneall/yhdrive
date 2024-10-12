40f7edc091613a7cd914cd0a50f2a58caf59ced6
runoneall <runoobsteve@126.com>
2024-10-13 04:21:35 +0800

    update files

--- cli.py
+++ cli.py
@@ -64,11 +64,45 @@ if mode == '1':
     print('上传完成')
 
 if mode == '2':
+    import json
     from AskYunhu import FileCtrl
+    import Tool
 
     # Load File List
-    MetaJsonList = os.listdir(MetaJsonFolder)
+    MetaJsonList = [file for file in os.listdir(MetaJsonFolder) if not file.startswith('.')]
+    if MetaJsonList == []:
+        print('没有可供下载的文件')
+        exit()
     print('\n     可下载列表')
     print('--------------------')
     for Item in MetaJsonList:
         print(f' {MetaJsonList.index(Item)+1}) {os.path.splitext(os.path.basename(Item))[0]}')
+    
+    # Pick File
+    FileId = int(input('\n下载: '))-1
+    FileName = MetaJsonList[FileId]
+    print(f'读取元信息 {FileName}')
+
+    # Read MetaJson
+    with open(f'{MetaJsonFolder}/{FileName}', 'r', encoding='utf-8') as metajson_file:
+        metajson = json.load(metajson_file)
+    target_name = metajson["name"]
+    target_chunks = metajson["chunks"]
+    print('\n     文件信息')
+    print('------------------')
+    print(f' 名称: {target_name}')
+    print(f' 块数量: {len(target_chunks)}')
+
+    # Download File
+    Tool.CleanDir()
+    if input('\n确定要下载吗? (Y/n) > ') == 'n':
+        print('取消下载')
+        exit()
+    with open(f"./cache/{target_name}", "wb") as file:
+        pass
+    for ChunkId in target_chunks:
+        print(f'下载块 {ChunkId}')
+        Content = FileCtrl.Download(ChunkId)
+        with open(f"./cache/{target_name}", "ab") as file:
+            file.write(Content)
+    print(f'文件已保存 ./cache/{target_name}')
